3cf37ed006e8aa51d2e5d2dba8c79e7c1998e1aa
Sadam Alfian <sadamalfianp@gmail.com>
2021-10-12 22:28:13 +0700

    add go-to-top button

--- site.js
+++ site.js
@@ -1,7 +1,27 @@
-// Menu selector
 let menuButton = document.querySelector('.hamburger');
 let navigationList = document.querySelector('.navigation__list');
+let topButton = document.getElementById('topBtn');
 
+// Menu selector
 menuButton.addEventListener('click', function () {
   navigationList.classList.toggle('show');
 });
+
+// top btn function
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topButton.style.display = 'block';
+  } else {
+    topButton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
