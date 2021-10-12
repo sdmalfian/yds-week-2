a648f4d3b902fd51c671e36dda14c3c0341d373e
Sadam Alfian <sadamalfianp@gmail.com>
2021-10-12 21:46:32 +0700

    Add responsive styling and mobile menu

--- site.js
+++ site.js
@@ -1,1 +1,7 @@
-// javascript code for index.html
+// Menu selector
+let menuButton = document.querySelector('.hamburger');
+let navigationList = document.querySelector('.navigation__list');
+
+menuButton.addEventListener('click', function () {
+  navigationList.classList.toggle('show');
+});
